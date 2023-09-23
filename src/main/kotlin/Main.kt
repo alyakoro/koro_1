92991495b9c099c8d96eb27b5c63947a78755085
1101-22 <alya.korobeynikova99@gmail.com>
2023-09-23 14:39:26 +0300

    удалить лишние строки

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,26 @@
+fun count2(input: String): String {
+    val res = StringBuilder()
+    var t = 1
+    for (i in 1 until input.length) {
+        if (input[i] == input[i - 1]) {
+            t++
+        } else {
+            res.append(input[i - 1])
+            if (t > 1) {
+                res.append(t)
+            }
+            t = 1
+        }
+    }
+    res.append(input[input.length - 1])
+    if (t > 1) {
+        res.append(t)
+    }
+    return res.toString()
+}
 fun main() {
-    println("Hello World!")
-
+    println("Введите строку символов: ")
+    val input = readLine().toString()
+    val output = count2(input)
+    println(output)
 }
